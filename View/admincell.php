282f766bb6edc4c7fb4e23d50c74f8a0bcdbef19
Steelwix <mhunmael@hotmail.com>
2022-05-26 16:53:16 +0200

    tABLE EDIT

--- View/admincell.php
+++ View/admincell.php
@@ -47,32 +47,43 @@ while ($data = $posts->fetch())
 </div>
     </div> 
     </div>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Contenu</th>
+      <th scope="col">Date du commentaire</th>
+      <th scope="col">Ecrit par</th>
+      <th scope="col">Sur le post</th>
+      <th scope="col">Options</th>
+    </tr>
+  </thead>
     <?php
 while ($com = $comments->fetch()){ ?>
-<br>
-<div class="container">
-    <div class="row">
-        <div class="col-11 postdata">
-        <p>Contenu :<strong><?= ($com['comment']) ?></strong>
-    <em>le <?= $com['comDate'] ?></em>
-    <em>Ecrit par l'utilisateur<strong>  <?= $com['username'] ?></em></strong>
-    <em>Sous le post  <?= $com['title'] ?></em>
-    </div>
-    <?php if($com['isValid'] == 0) 
+ <tbody>
+    <tr>
+      <td><?= ($com['comment']) ?></td>
+      <td><?= ($com['comDate']) ?></td>
+      <td><?= ($com['username']) ?></td>
+      <td><?= ($com['title']) ?></td>
+      <?php if($com['isValid'] == 0) 
  {
-        ?>    
-            <div class="col-1 postdata">
-               
-        <a href="index.php?action=deleteComment&amp;idComment=<?= $com['idComment'] ?>"class="btn btn-danger">Effacer ce commentaire</a>   
-            <a href="index.php?action=commentIsValid&amp;idComment=<?= $com['idComment'] ?>"class="btn btn-success">Valider ce commentaire</a></div>
+       ?>   
+        <td><a href="index.php?action=deleteComment&amp;idComment=<?= $com['idComment'] ?>"class="btn btn-danger">Effacer</a></td>   
+        <td><a href="index.php?action=commentIsValid&amp;idComment=<?= $com['idComment'] ?>"class="btn btn-success">Valider</a></td> 
         <?php
     }
     else {
-       ?> <div class="col-1 postdata">
-        <a href="index.php?action=deleteComment&amp;idComment=<?= $com['idComment'] ?>"class="btn btn-danger">Effacer ce commentaire</a></div> 
+       ?>
+        <td><a href="index.php?action=deleteComment&amp;idComment=<?= $com['idComment'] ?>"class="btn btn-danger">Effacer</a> </td> 
            <?php
-    }} ?>
-</div></div>
+    } ?>
+    </td>
+    <?php } ?>
+    </tr>
+  </tbody>
+</table>
+
+ 
         <div class="container">
     <div class="row">
         <div class="col-12  text-center">
@@ -83,62 +94,36 @@ while ($com = $comments->fetch()){ ?>
     <table class="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Pseudo</th>
+      <th scope="col">Email</th>
+      <th scope="col">Création du compte</th>
+      <th scope="col">Droits</th>
+      <th scope="col">Options</th>
     </tr>
   </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-        <div class="container">
-    <div class="row">
-        <div class="col-3 posttitle"><b>Pseudo</b></div>
-        <div class="col-3 posttitle"><b>email</b></div>
-        <div class="col-3 posttitle"><b>création</b></div>
-        <div class="col-3 posttitle"><b>droits</b></div>
-       
-    </div>
-<?php
+  <?php
 while ($com = $users->fetch()){
     if ($com['isAdmin']==1){
         $com['isAdmin'] = "administrateur";
     } 
     else { $com['isAdmin'] = "utilisateur";
     }?>
-
-    <div class="row">
-        <div class="col-3 postdata text-left"><?= ($com['username']) ?></div>
-        <div class="col-3 postdata text-left"><?= $com['email'] ?></div>
-        <div class="col-3 postdata text-left"><?= $com['created_at'] ?></div>
-        <div class="col-3 postdata text-left"><?= $com['isAdmin'] ?></div>
-         <div class="col-1">
-    <em><a href="index.php?action=inspectUser&amp;id=<?= $com['id'] ?>" class="btn btn-danger">Supprimer</a></em></div>
-    <div class="col-1">
-    <em><a href="index.php?action=editUserAdmin&amp;id=<?= $com['id'] ?>" class="btn btn-primary">Editer</a></em>
-
-
-        </div></div></div>
-
+  <tbody>
+    <tr>
+      <td><?= ($com['username']) ?></td>
+      <td><?= ($com['email']) ?></td>
+      <td><?= ($com['created_at']) ?></td>
+      <td><?= ($com['isAdmin']) ?></td>
+      <td><a href="index.php?action=inspectUser&amp;id=<?= $com['id'] ?>" class="btn btn-danger">Supprimer</a>
+      <a href="index.php?action=editUserAdmin&amp;id=<?= $com['id'] ?>" class="btn btn-primary">Editer</a>
+    </td>
     <?php } ?>
+    </tr>
+  </tbody>
+</table>
+
+
+   
 
     
 <?php
