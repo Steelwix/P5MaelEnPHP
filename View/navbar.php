e99de741a3ba9073dee01e3e4c7c0a8a6b66b0bc
Steelwix <mhunmael@hotmail.com>
2022-06-13 17:16:03 +0200

    navbar, index on title

--- View/navbar.php
+++ View/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Mael En PHP</a>
+    <a class="navbar-brand" href="index.php">Mael En PHP</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -12,11 +12,6 @@
 
       <div class="collapse navbar-collapse" id="navbarColor02">
         <ul class="navbar-nav me-auto">
-          <li class="nav-item">
-            <a class="nav-link active" href="index.php">Accueil
-              <span class="visually-hidden">(current)</span>
-            </a>
-          </li>
           <li class="nav-item">
             <a class="nav-link" href="index.php?action=contact">Contact</a>
           </li>
@@ -43,11 +38,6 @@
 
           <div class="collapse navbar-collapse" id="navbarColor02">
             <ul class="navbar-nav me-auto">
-              <li class="nav-item">
-                <a class="nav-link active" href="index.php">Accueil
-                  <span class="visually-hidden">(current)</span>
-                </a>
-              </li>
               <li class="nav-item">
                 <a class="nav-link" href="index.php?action=login&amp;">Se connecter</a>
               </li>
